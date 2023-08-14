c7f759ecc7bee2096101b20e1081628ba638a582
MohsenKhashei <m.khasheii@gmail.com>
2023-08-14 10:10:29 +0300

    add checking key validation in middleware

--- libs/common/src/middleware/authentication.middleware.ts
+++ libs/common/src/middleware/authentication.middleware.ts
@@ -3,24 +3,50 @@ import {
   Injectable,
   LoggerService,
   NestMiddleware,
+  UnauthorizedException,
 } from '@nestjs/common';
+import { AppService } from 'apps/hyper-bridge/src/app.service';
+import { EncryptedDataDto } from 'apps/hyper-bridge/src/dto/encrypted.data.dto';
+import { HeaderKeyDto } from 'apps/hyper-bridge/src/dto/header.key.dto';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 import { NextFunction, Request, Response } from 'express';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-// import { RequestService } from 'src/request.service';
 
 @Injectable()
 export class AuthenticationMiddleware implements NestMiddleware {
   constructor(
-    // private readonly requestService: RequestService
     @Inject(WINSTON_MODULE_NEST_PROVIDER)
     private readonly logger: LoggerService,
+    private readonly appService: AppService,
   ) {}
 
-  use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(AuthenticationMiddleware.name);
+  async use(req: Request, res: Response, next: NextFunction) {
+    const requestBody = plainToClass(EncryptedDataDto, req.body);
+    const requestBodyError = await validate(requestBody);
+    const headerKey = plainToClass(HeaderKeyDto, req.headers);
+    const headerKeyError = await validate(headerKey);
 
-    const userId = '124';
-    // this.requestService.setUserId(userId);
+    if (requestBodyError.length > 0) {
+      this.logger.error(`${AuthenticationMiddleware.name} requestBodyError`);
+      throw new UnauthorizedException('The Posted Data Is Not Acceptable');
+    }
+    if (headerKeyError.length > 0) {
+      this.logger.error(`${AuthenticationMiddleware.name} ${headerKeyError}`);
+      throw new UnauthorizedException(
+        'The key Parameter Not Existed In Header',
+      );
+    }
+
+    const keyIsValid = await this.appService.checkPublicKeyIsValid(
+      headerKey.key,
+    );
+    if (!keyIsValid) {
+      this.logger.error(
+        `${AuthenticationMiddleware.name} key is not exist in DB or is not valid.`,
+      );
+      throw new UnauthorizedException('key is not valid.');
+    }
 
     next();
   }
